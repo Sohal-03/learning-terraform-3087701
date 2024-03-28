d6914886d026a71ae5e49284788fbad3f9818173
Sohal-03 <sohal.learn.it@gmail.com>
2024-03-28 03:44:21 -0600

    +added cidr blocks to the security groups
    The security groups need cidr blocks to run in terraform

--- main.tf
+++ main.tf
@@ -37,6 +37,7 @@ resource "aws_security_group_rule" "http_in" {
   from_port = 80
   to_port = 80
   protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
